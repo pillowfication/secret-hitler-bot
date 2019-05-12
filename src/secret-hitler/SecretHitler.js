92d791b017c081f4217da51534a95d8e35bdca99
Markus Tran <thebluepillow@gmail.com>
2019-05-11 18:51:27 -0700

    add some voting

--- src/secret-hitler/SecretHitler.js
+++ src/secret-hitler/SecretHitler.js
@@ -1,14 +1,18 @@
 const State = {
   PENDING: 'pending',
   ELECTION: 'election',
+  ELECTION_VOTE: 'election vote',
   LEGISLATIVE_SESSION: 'legislative session',
   EXECUTIVE_ACTION: 'executive action'
 }
-
-const Roles = {
+const Role = {
   HITLER: 'hitler',
   FACIST: 'facist',
   LIBERAL: 'liberal'
+}
+const Vote = {
+  YES: 'yes',
+  NO: 'no'
 }
 
 function shuffle (arr) {
@@ -25,7 +29,7 @@ module.exports = class SecretHitler {
   }
 
   initialize (users) {
-    const { HITLER, FACIST, LIBERAL } = Roles
+    const { HITLER, FACIST, LIBERAL } = Role
     const roles = shuffle(({
       5: [ HITLER, FACIST, LIBERAL, LIBERAL, LIBERAL ],
       6: [ HITLER, FACIST, LIBERAL, LIBERAL, LIBERAL, LIBERAL ],
@@ -34,13 +38,86 @@ module.exports = class SecretHitler {
       9: [ HITLER, FACIST, FACIST, FACIST, LIBERAL, LIBERAL, LIBERAL, LIBERAL, LIBERAL ],
       10: [ HITLER, FACIST, FACIST, FACIST, LIBERAL, LIBERAL, LIBERAL, LIBERAL, LIBERAL, LIBERAL ]
     })[users.size])
-    this.players = users.array().map((user, index) => ({
+
+    this.players = shuffle(users.array().map((user, index) => ({
       user,
-      role: roles[index]
-    }))
+      role: roles[index],
+      isAlive: true
+    })))
     this.state = State.ELECTION
+    this.presidentPlacard = 0
+    this.previousPresident = null
+    this.previousChancellor = null
+    this.failedGovernmentsCount = 0
+    this.chancellorCandidate = null
+  }
+
+  _findPlayer (user) {
+    return this.players.find(player => player.user.id === user.id)
+  }
+
+  getPlayers () {
+    return this.players.map(player => player.user)
+  }
+
+  getAlivePlayers () {
+    return this.players.filter(player => player.isAlive).map(player => player.user)
+  }
+
+  getPlayerRole (user) {
+    return this._findPlayer(user).role
+  }
+
+  getPlayersWithRole (role) {
+    return this.players
+      .filter(player => player.role === role)
+      .map(player => player.user)
+  }
+
+  getPlayerById (id) {
+    const player = this._findPlayer({ id })
+    return player && player.user
+  }
+
+  getPresidentialCandidate () {
+    return this.players[this.presidentPlacard].user
+  }
+
+  getIneligibleCandidates () {
+    const ineligibleCandidates = []
+    if (this.previousPresident && this.previousPresident.isAlive) {
+      ineligibleCandidates.push(this.previousPresident)
+    }
+    if (this.previousChancellor && this.previousChancellor.isAlive) {
+      ineligibleCandidates.push(this.previousChancellor)
+    }
+    return ineligibleCandidates
+  }
+
+  getFailedGovernmentsCount () {
+    return this.failedGovernmentsCount
+  }
+
+  isAlive (user) {
+    return this._findPlayer(user).isAlive
+  }
+
+  isEligible (user) {
+    return this.isAlive(user) &&
+      (this.previousPresident === null || this.previousPresident.id !== user.id) &&
+      (this.previousChancellor === null || this.previousChancellor.id !== user.id)
+  }
+
+  nominateChancellor (user) {
+    this.state = State.ELECTION_VOTE
+    this.chancellorCandidate = user
+  }
+
+  voteGovernment (votes) {
+
   }
 }
 
 module.exports.State = State
-module.exports.Roles = Roles
+module.exports.Role = Role
+module.exports.Vote = Vote
--- src/secret-hitler/index.js
+++ src/secret-hitler/index.js
@@ -5,11 +5,15 @@ const SecretHitler = require('./SecretHitler')
 const parseArgs = require('../parseArgs')
 
 const bearFactsTxt = fs.readFileSync(path.resolve(__dirname, './bear-facts.txt'))
-const { State, Roles } = SecretHitler
+const { State, Role, Vote } = SecretHitler
+
+class AssertionError extends Error {}
 
 module.exports = function secretHitler (client) {
   const command = client.config.commandPrefix + client.config.commandSecretHitler
   const participationEmoji = '✅'
+  const voteYesEmoji = '✅'
+  const voteNoEmoji = '❌'
   const games = new Map()
   const bearFacts = bearFactsTxt.toString().split('\n')
   bearFacts.pop()
@@ -22,148 +26,266 @@ module.exports = function secretHitler (client) {
 
     const { channel, author } = message
     const game = games.get(channel.id)
+    const sh = game && game.sh
 
-    ;(async () => {
+    async function assert (condition, message) {
+      if (!condition) {
+        await channel.send(message)
+        throw new AssertionError()
+      }
+    }
+
+    (async () => {
       switch (args[1]) {
         case 'start':
-          if (game) {
-            await channel.send(
-              'A game has already been started in this channel. ' +
-              `Type \`${command} stop\` to stop it.`
+          await assert(!game,
+            'A game has already been started in this channel.\n' +
+            `Type \`${command} stop\` to stop it.`
+          )
+
+          games.set(channel.id, { sh: new SecretHitler() })
+          await channel
+            .send(
+              'Starting a game of Secret Hitler. ' +
+              `React to this message with a ${participationEmoji} to join.\n` +
+              `Type \`${command} confirm\` to begin.`,
+              new Discord.Attachment(path.resolve(__dirname, './images/banner.png'))
             )
-          } else {
-            games.set(channel.id, { sh: new SecretHitler() })
-            await channel
-              .send(
-                'Starting a game of Secret Hitler. ' +
-                `React to this message with a ${participationEmoji} to join. ` +
-                `Then type \`${command} confirm\` to begin.`,
-                new Discord.Attachment(path.resolve(__dirname, './images/banner.png'))
-              )
-              .then(message => {
-                message.react(participationEmoji)
-                games.get(channel.id).participationMessage = message
-              })
-          }
+            .then(message => {
+              message.react(participationEmoji)
+              games.get(channel.id).participationMessage = message
+            })
           break
         case 'confirm':
-          if (game) {
-            const { participationMessage } = games.get(channel.id)
-            if (!participationMessage) {
-              throw new Error(
-                `A game has started in channel ${channel.id}, ` +
-                'but the `participationMessage` could not be found.'
-              )
-            }
+          await assert(game,
+            'A game has not been started in this channel.\n' +
+            `Type \`${command} start\` to start one.`
+          )
+          await assert(sh.state === State.PENDING,
+            'A game is already in play in this channel.\n' +
+            `Type \`${command} help\` for more information.`
+          )
 
-            const participationReaction = participationMessage.reactions
-              .find(messageReaction => messageReaction.emoji.name === participationEmoji)
-            if (!participationReaction) {
-              throw new Error(
-                `A game has started in channel ${channel.id}, ` +
-                `but the ${participationEmoji} reaction on \`participationMessage\` could not be found.`
-              )
-            }
-
-            const players = participationReaction.users.filter(user => !user.bot)
-            if (players.size < 5) {
-              await channel.send(
-                `Only ${players.size} ${players.size === 1 ? 'user has' : 'users have'} joined the match, and at least 5 are needed. ` +
-                'Game has not been started.\n' +
-                `\`\`\`${players.map(user => user.tag).join(', ')}\n\`\`\``
-              )
-            } else if (players.size > 10) {
-              await channel.send(
-                `${players.size} users have joined the match, but at most 10 can play. ` +
-                'Game has not been started.\n' +
-                `\`\`\`${players.map(user => user.tag).join(', ')}\n\`\`\``
-              )
-            } else {
-              game.sh.initialize(players)
-              await channel.send(
-                `A game has been started with ${players.size} players. ` +
-                'Roles are being sent out...'
-              )
-
-              const hitlers = game.sh.players.filter(player => player.role === Roles.HITLER)
-              const facists = game.sh.players.filter(player => player.role === Roles.FACIST)
-              for (const player of game.sh.players) {
-                const bearFact = bearFacts[bearFacts.length * Math.random() | 0]
-                switch (player.role) {
-                  case Roles.HITLER:
-                    if (players.size <= 6) {
-                      player.user.send(
-                        'You are **Hitler (Facist)**.\n' +
-                        `Your fellow Facist is ${facists[0].user.tag}. ` +
-                        'You two know each other’s roles.\n\n' +
-                        `Bear Fact: ${bearFact}`
-                      )
-                    } else {
-                      player.user.send(
-                        'You are **Hitler (Facist)**.\n' +
-                        'You don’t know who is Facist and who is Liberal. ' +
-                        'Your fellow Facists know that you are Hitler.\n\n' +
-                        `Bear Fact: ${bearFact}`
-                      )
-                    }
-                    break
-                  case Roles.FACIST:
-                    if (players.size <= 6) {
-                      player.user.send(
-                        'You are **Facist**.\n' +
-                        `Hitler is ${hitlers[0].user.tag}. ` +
-                        'You two know each other’s roles.\n\n' +
-                        `Bear Fact: ${bearFact}`
-                      )
-                    } else {
-                      const otherFacists = facists.filter(facist => facist.user.id !== player.user.id)
-                      player.user.send(
-                        'You are **Facist**.\n' +
-                        `Hitler is ${hitlers[0].user.tag}. ` +
-                        `Your fellow ${players.size <= 8 ? 'Facist is' : 'Facists are'} ${otherFacists.map(facist => facist.user.tag).join(', ')}. ` +
-                        'Hitler does not know who the Facists are.\n\n' +
-                        `Bear Fact: ${bearFact}`
-                      )
-                    }
-                    break
-                  case Roles.LIBERAL:
-                    player.user.send(
-                      'You are **Liberal**.\n' +
-                      'You don’t know who the other Facists and Liberals are. ' +
-                      'The Facists know that you are Liberal.\n\n' +
-                      `Bear Fact: ${bearFact}`
-                    )
-                    break
-                }
-              }
-            }
-          } else {
-            await channel.send(
-              'A game has not been started in this channel. ' +
-              `Type \`${command} start\` to start one.`
+          const { participationMessage } = game
+          if (!participationMessage) {
+            throw new Error(
+              `A game has started in channel ${channel.id}, ` +
+              'but the `participationMessage` could not be found.'
             )
           }
+          const participationReaction = participationMessage.reactions
+            .find(messageReaction => messageReaction.emoji.name === participationEmoji)
+          if (!participationReaction) {
+            throw new Error(
+              `A game has started in channel ${channel.id}, ` +
+              `but the ${participationEmoji} reaction on \`participationMessage\` could not be found.`
+            )
+          }
+
+          const players = participationReaction.users.filter(user => !user.bot)
+          // await assert(players.size >= 5,
+          //   `Only ${players.size} ${players.size === 1 ? 'user has' : 'users have'} joined the match, and at least 5 are needed.\n` +
+          //   'Game has not been started.\n' +
+          //   `\`\`\`\n${players.map(user => user.tag).join('\n')}\n\`\`\``
+          // )
+          await assert(players.size <= 10,
+            `${players.size} users have joined the match, but at most 10 can play.\n` +
+            'Game has not been started.\n' +
+            `\`\`\`\n${players.map(user => user.tag).join('\n')}\n\`\`\``
+          )
+
+          sh.initialize(players)
+          await channel.send(
+            `A game has been started with ${players.size} players. ` +
+            'Roles are being sent out to each player...'
+          )
+
+          const hitlers = sh.getPlayersWithRole(Role.HITLER)
+          const facists = sh.getPlayersWithRole(Role.FACIST)
+          await Promise.all(sh.getPlayers().map(user => {
+            const bearFact = bearFacts[bearFacts.length * Math.random() | 0]
+            switch (sh.getPlayerRole(user)) {
+              case Role.HITLER:
+                if (players.size <= 6) {
+                  return user.send(
+                    'You are **Hitler (Facist)**.\n' +
+                    `Your fellow Facist is ${facists[0].tag}. ` +
+                    'You two know each other’s roles.\n\n' +
+                    `Bear Fact: ${bearFact}`
+                  )
+                } else {
+                  return user.send(
+                    'You are **Hitler (Facist)**.\n' +
+                    'You don’t know who is Facist and who is Liberal. ' +
+                    'Your fellow Facists know that you are Hitler.\n\n' +
+                    `Bear Fact: ${bearFact}`
+                  )
+                }
+              case Role.FACIST:
+                if (players.size <= 6) {
+                  return user.send(
+                    'You are **Facist**.\n' +
+                    `Hitler is ${hitlers[0].tag}. ` +
+                    'You two know each other’s roles.\n\n' +
+                    `Bear Fact: ${bearFact}`
+                  )
+                } else {
+                  const otherFacists = facists.filter(facist => facist.user.id !== user.id)
+                  return user.send(
+                    'You are **Facist**.\n' +
+                    `Hitler is ${hitlers[0].tag}. ` +
+                    `Your fellow ${players.size <= 8 ? 'Facist is' : 'Facists are'} ${otherFacists.map(facist => facist.user.tag).join(', ')}. ` +
+                    'Hitler does not know who the Facists are.\n\n' +
+                    `Bear Fact: ${bearFact}`
+                  )
+                }
+              case Role.LIBERAL:
+                return user.send(
+                  'You are **Liberal**.\n' +
+                  'You don’t know who the other Facists and Liberals are. ' +
+                  'The Facists know that you are Liberal.\n\n' +
+                  `Bear Fact: ${bearFact}`
+                )
+            }
+          }))
+
+          const presidentialCandidate = sh.getPresidentialCandidate()
+          await channel.send(
+            'Roles have been assigned! ' +
+            `The turn order is:\n\`\`\`\n${sh.getPlayers().map((user, index) =>
+              `${index < 9 ? ' ' : ''}${index + 1}. ${user.tag}`
+            ).join('\n')}\n\`\`\`\n\n` +
+            `<@${presidentialCandidate.id}> is the first Presidential Candidate.\n` +
+            `Type \`${command} nominate @user\` to nominate a Chancellor.`
+          )
+          break
+        case 'nominate':
+          await assert(game,
+            'A game has not been started in this channel.\n' +
+            `Type \`${command} start\` to start one.`
+          )
+          await assert(sh.state === State.ELECTION,
+            'It is not time to nominate a Chancellor.\n' +
+            `Type \`${command} help\` for more information.`
+          )
+          await assert(sh.getPresidentialCandidate().id === author.id,
+            'You are not the current Presidential Candidate.\n' +
+            `Type \`${command} help\` for more information.`
+          )
+
+          const nominee = message.mentions.users.first()
+          await assert(nominee,
+            'You must mention someone to nominate as Chancellor.\n' +
+            `Type \`${command} help\` for more information.`
+          )
+          await assert(nominee.id !== author.id,
+            `You cannot nominate yourself for Chancellor.\n` +
+            `Type \`${command} help\` for more information.`
+          )
+          await assert(sh.getPlayerById(nominee.id),
+            `<@${nominee.id}> is not a player of the current game.\n` +
+            `Type \`${command} help\` for more information.`
+          )
+          await assert(sh.isAlive(nominee),
+            `<@${nominee.id}> is not alive in the current game.\n` +
+            `Type \`${command} help\` for more information.`
+          )
+          await assert(sh.isEligible(nominee),
+            `<@${nominee.id}> is not eligible to be a Chancellor.\n` +
+            `Type \`${command} help\` for more information.`
+          )
+
+          sh.nominateChancellor(nominee)
+          await channel.send(
+            'The proposed government is:\n' +
+            ` - President: <@${author.id}>\n` +
+            ` - Chancellor: <@${nominee.id}>\n\n` +
+            'Ballots are being sent out to each player...'
+          )
+
+          game.votes = {}
+          await Promise.all(sh.getAlivePlayers().map(user => (async () => {
+            const message = await user.send(
+              'The proposed government is:\n' +
+              ` - President: <@${author.id}>\n` +
+              ` - Chancellor: <@${nominee.id}>\n\n` +
+              `React to this message with a ${voteYesEmoji} or ${voteNoEmoji} to vote.`
+            )
+            await message.react(voteYesEmoji)
+            await message.react(voteNoEmoji)
+            return new Promise((resolve, reject) => {
+              const handler = (messageReaction, user) => {
+                (async () => {
+                  if (!user.bot && messageReaction.message.id === message.id) {
+                    if (messageReaction.emoji.name === voteYesEmoji || messageReaction.emoji.name === voteNoEmoji) {
+                      switch (messageReaction.emoji.name) {
+                        case voteYesEmoji:
+                          game.votes[user.id] = Vote.YES
+                          await user.send('You have voted for this government to **pass**.')
+                          break
+                        case voteNoEmoji:
+                          game.votes[user.id] = Vote.NO
+                          await user.send('You have voted for this government to **fail**.')
+                          break
+                      }
+                      client.removeListener('messageReactionAdd', handler)
+                      resolve()
+                    }
+                  }
+                })()
+                  .catch(err => {
+                    client.removeListener('messageReactionAdd', handler)
+                    reject(err)
+                  })
+              }
+              client.on('messageReactionAdd', handler)
+            })
+          })()))
+
+          await channel.send(
+            'The votes are in:\n' +
+            `\`\`\`diff\n${sh.getAlivePlayers().map(user =>
+              `${({ [Vote.YES]: '+ ', [Vote.NO]: '- ' })[game.votes[user.id]]} ${user.tag}`
+            ).join('\n')}\n\`\`\``
+          )
+          sh.voteGovernment(game.votes)
           break
         case 'help':
-          if (game) {
-            switch (game.sh.state) {
-              case State.PENDING:
-                await channel.send(
-                  'A game is currently being created. ' +
-                  `To join it, react to the message linked below with a ${participationEmoji}. ` +
-                  `To start the game, type \`${command} confirm\`.\n\n` +
-                  game.participationMessage.url
-                )
-                break
-            }
-          } else {
-            await channel.send(
-              'A game has not been started in this channel. ' +
-              `Type \`${command} start\` to start one.`
-            )
+          await assert(game,
+            'A game has not been started in this channel. ' +
+            `Type \`${command} start\` to start one.`
+          )
+
+          switch (sh.state) {
+            case State.PENDING:
+              await channel.send(
+                'A game is currently being created. ' +
+                `To join it, react to the message linked below with a ${participationEmoji}.\n` +
+                `To start the game, type \`${command} confirm\`.\n\n` +
+                game.participationMessage.url
+              )
+              break
+            case State.ELECTION:
+              const presidentialCandidate = sh.getPresidentialCandidate()
+              const ineligibleCandidates = sh.getIneligibleCandidates()
+              const failedGovernmentsCount = sh.getFailedGovernmentsCount()
+              await channel.send(
+                `It is <@${presidentialCandidate.id}>’s turn to nominate a Chancellor.\n` +
+                (ineligibleCandidates.length > 0
+                  ? `The Chancellor cannot be ${ineligibleCandidates.map(candidate => `<@${candidate.id}>`).join(' or ')}.\n`
+                  : '') +
+                `There have been ${failedGovernmentsCount} failed ${failedGovernmentsCount === 1 ? 'government' : 'governments'}.\n\n` +
+                `Type \`${command} nominate @user\` to nominate a Chancellor.`
+              )
+              break
           }
           break
         case 'stop':
+          assert(game,
+            'A game has not been started in this channel.\n' +
+            `Type \`${command} start\` to start one.`
+          )
+
           games.delete(channel.id)
           await channel.send('Game deleted.')
           break
@@ -173,10 +295,12 @@ module.exports = function secretHitler (client) {
           break
       }
     })().catch(err => {
-      channel
-        .send(`Something bad has happened\n\`\`\`\n${err.message}\n\`\`\``)
-        .catch(() => {})
-      console.error(err)
+      if (!(err instanceof AssertionError)) {
+        channel
+          .send(`Something bad has happened\n\`\`\`diff\n - ${err.message}\n\`\`\``)
+          .catch(() => {})
+        console.error(err)
+      }
     })
   })
 }
